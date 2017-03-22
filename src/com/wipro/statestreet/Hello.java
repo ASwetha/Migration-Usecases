b6782c09b3a8d084072591e1c96c53c0ca77ddba
ProductionUser <productionuser@gmail.com>
2017-03-22 18:01:12 +0530

    Adding new packages and two class to the mavenized Ant Appliction in dev1-branch

---
+++ src/com/wipro/statestreet/Hello.java
@@ -0,0 +1,9 @@
+package com.wipro.statestreet;
+
+public class Hello {
+
+	public void printName(){
+		String name="CC2Git";
+		System.out.println(name);
+	}
+}
---
+++ src/com/wipro/statestreet/MainClass.java
@@ -0,0 +1,11 @@
+package com.wipro.statestreet;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Hello hello = new Hello();
+		hello.printName();
+	}
+
+}
